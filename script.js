80ecc1860db280ba676c9f01bc1d9b82f9329c05
valencsoares <thigasgui@gmail.com>
2024-10-26 19:56:13 -0400

    Criado a função de fazer média entre 2 números

---
+++ script.js
@@ -0,0 +1,20 @@
+<<<<<<< Updated upstream
+=======
+let somar = function () {
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value
+    let saida = document.getElementById("saida1")
+
+    let soma = parseFloat(num1) + parseFloat(num2)
+    saida.innerHTML = soma
+}
+
+let mediaArit = function () {
+    let n1 = document.getElementById("valor1").value
+    let n2 = document.getElementById("valor2").value
+    let saida = document.getElementById("saida2")
+
+    let media = (parseFloat(n1) + parseFloat(n2)) /2
+    saida.innerHTML = media
+}
+>>>>>>> Stashed changes
